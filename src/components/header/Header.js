91a202840c6cc678ec06e8a924d407a5c8ad73de
morningzombie <terri@terribailey.com>
2020-04-24 15:31:35 -0400

    working on faves

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-import qs from 'qs';
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import qs from "qs";
 
 const Header = ({ login }) => {
   return (
@@ -21,16 +21,6 @@ const Header = ({ login }) => {
       </button>
       <div className="collapse navbar-collapse" id="navbarText">
         <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/#">
-              Home <span className="sr-only">(current)</span>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/register">
-              Create New Account
-            </Link>
-          </li>
           {/* <li className="nav-item">
             <Link className="nav-link" to="/profile">
               profile
@@ -43,6 +33,12 @@ const Header = ({ login }) => {
           </li> */}
         </ul>
         <form className="form-inline my-2 my-lg-0">
+          <Link
+            className="btn btn-outline-success my-2 my-sm-0 mr-2"
+            to="/register"
+          >
+            Create New Account
+          </Link>
           <Link
             to="/login"
             className="btn btn-outline-success my-2 my-sm-0"
