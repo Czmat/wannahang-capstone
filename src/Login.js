795aa8d2505a56dd3e92ba06b0505ea9d1760bf4
morningzombie <terri@terribailey.com>
2020-04-25 00:44:54 -0400

    the styling has begun

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
 
 const Login = ({ login }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const history = useHistory();
-  const goToUpload = () => history.push('/file/upload');
+  const goToUpload = () => history.push("/file/upload");
 
   const onSubmit = (ev) => {
     ev.preventDefault();
@@ -19,46 +19,92 @@ const Login = ({ login }) => {
   };
 
   return (
-    <div className="container w-50 d-flex justify-content-center">
-      <div className=" ">
-        <form className="" onSubmit={onSubmit}>
-          <h1>Login</h1>
-          {error ? (
-            <p className="alert  alert-danger error " role="alert">
-              {error}
-            </p>
-          ) : (
-            ''
-          )}
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              value={email}
-              onChange={(ev) => setEmail(ev.target.value)}
-            />
-            <small id="emailHelp" className="form-text text-muted">
-              We'll never share your email with anyone else.
-            </small>
+    <div className="">
+      <header className="v-header container-wrap w-50 d-flex justify-content-center">
+        <div className="fullscreen-video-wrap">
+          <video
+            src="http://www.terribailey.com/images/Roller-Coaster.mp4"
+            autoPlay={true}
+            loop={true}
+            muted
+          ></video>
+        </div>
+        <div className="header-overlay"></div>
+        <div className="header-content text-md-center">
+          <h1>WannaHang</h1>{" "}
+          <p>
+            The online community of friends you have yet to meet!
+            <br />
+            Login{" "}
+          </p>
+          <div className=" ">
+            <form className="" onSubmit={onSubmit}>
+              {error ? (
+                <p className="alert  alert-danger error " role="alert">
+                  {error}
+                </p>
+              ) : (
+                ""
+              )}
+              <div className="form-group">
+                <label htmlFor="exampleInputEmail1">Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  value={email}
+                  onChange={(ev) => setEmail(ev.target.value)}
+                />
+                <small id="emailHelp" className="form-text text-muted ">
+                  We'll never share your email with anyone else.
+                </small>
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  value={password}
+                  onChange={(ev) => setPassword(ev.target.value)}
+                />
+              </div>
+              <button className="btn btn-primary">Log In</button>
+            </form>
+            <Link to="/register">Create Account</Link>
           </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-              value={password}
-              onChange={(ev) => setPassword(ev.target.value)}
-            />
-          </div>
-          <button className="btn btn-primary">Log In</button>
-        </form>
-        <hr />
-        <Link to="/register">Create Account</Link>
-      </div>
+        </div>
+      </header>
+
+      <section className="section section-a">
+        <div className="container">
+          <h2>WannaHang</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde,
+            impedit amet minima iste autem cumque et maiores blanditiis
+            doloribus aut dolorum quaerat non est voluptatum, tempore ut dolorem
+            voluptas quod quae accusantium, ex inventore ducimus. Beatae
+            mollitia exercitationem, quam similique, consectetur ratione
+            reprehenderit delectus neque eligendi facere soluta dolor ducimus!
+          </p>
+          <p>©2020 The TEC Group</p>
+        </div>
+      </section>
+
+      {/* <section className="section section-b">
+        <div className="container">
+          <h2>Section B</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde,
+            impedit amet minima iste autem cumque et maiores blanditiis
+            doloribus aut dolorum quaerat non est voluptatum, tempore ut dolorem
+            voluptas quod quae accusantium, ex inventore ducimus. Beatae
+            mollitia exercitationem, quam similique, consectetur ratione
+            reprehenderit delectus neque eligendi facere soluta dolor ducimus!
+          </p>
+        </div>
+      </section> */}
     </div>
   );
 };
--- src/components/User/CreateNewUser.js
+++ src/components/User/CreateNewUser.js
@@ -1,26 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+import { Player } from "video-react";
 
 const CreateNewUser = ({ login }) => {
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [zipcode, setZipcode] = useState('');
-  const [birthday, setBirthday] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [gender, setGender] = useState('');
-  const [error, setError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [zipcode, setZipcode] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [gender, setGender] = useState("");
+  const [error, setError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const history = useHistory();
-  const goToUploadPhoto = () => history.push('/file/upload');
+  const goToUploadPhoto = () => history.push("/file/upload");
 
   const createUser = (user) => {
-    axios.post('/api/users', user).then((response) => {
+    axios.post("/api/users", user).then((response) => {
       login({ email, password }).catch((ex) =>
         setError(ex.response.data.message)
       );
@@ -47,97 +48,124 @@ const CreateNewUser = ({ login }) => {
     }
   };
   return (
-    <div className="container-sm">
-      <hr />
-      <h1>Join the Fam!</h1>
-      <form className="w-50" onSubmit={onSubmit}>
-        <div className="row form-group">
-          <div className="col">
-            <input
-              value={firstname}
-              type="text"
-              className="form-control"
-              placeholder="First name"
-              onChange={(ev) => setFirstname(ev.target.value)}
-            />
-          </div>
-          <div className="col">
-            <input
-              value={lastname}
-              type="text"
-              className="form-control"
-              placeholder="Last name"
-              onChange={(ev) => setLastname(ev.target.value)}
-            />
-          </div>
+    <div>
+      <header className="v-header container-wrap w-50 d-flex justify-content-center">
+        <div className="fullscreen-video-wrap">
+          <video
+            src="http://www.terribailey.com/images/Roller-Coaster.mp4"
+            autoPlay={true}
+            loop={true}
+            muted
+          ></video>
         </div>
-        <div className="form-group">
-          <input
-            value={username}
-            className="form-control"
-            type="text"
-            placeholder="Username"
-            onChange={(ev) => setUsername(ev.target.value)}
-          />
+        <div className="header-overlay"></div>
+        <div className="header-content text-md-center">
+          <h1>WannaHang</h1>
+          <p>Sign up today and find a new friend</p>
+          <form className="text-center" onSubmit={onSubmit}>
+            <div className="row form-group">
+              <div className="col">
+                <input
+                  value={firstname}
+                  type="text"
+                  className="form-control"
+                  placeholder="First name"
+                  onChange={(ev) => setFirstname(ev.target.value)}
+                />
+              </div>
+              <div className="col">
+                <input
+                  value={lastname}
+                  type="text"
+                  className="form-control"
+                  placeholder="Last name"
+                  onChange={(ev) => setLastname(ev.target.value)}
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <input
+                value={username}
+                className="form-control"
+                type="text"
+                placeholder="Username"
+                onChange={(ev) => setUsername(ev.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                value={phone}
+                className="form-control"
+                type="text"
+                placeholder="phone number"
+                onChange={(ev) => setPhone(ev.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                value={email}
+                type="email"
+                className="form-control"
+                id="inputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="email address"
+                onChange={(ev) => setEmail(ev.target.value)}
+              />
+              <small id="emailHelp" className="form-text text-muted">
+                We'll never share your email with anyone else.
+              </small>
+            </div>
+            <div className="form-group">
+              <input
+                value={password}
+                type="password"
+                className="form-control"
+                id="inputPassword1"
+                placeholder="password"
+                onChange={(ev) => setPassword(ev.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                value={confirmPassword}
+                type="password"
+                className="form-control"
+                id="inputConfirmPassword1"
+                placeholder="confirm password"
+                onChange={(ev) => setConfirmPassword(ev.target.value)}
+              />
+              <div>{passwordError}</div>
+            </div>
+            <button
+              className="btn btn-success my-2 my-sm-0"
+              disabled={
+                !firstname ||
+                !lastname ||
+                !email ||
+                !phone ||
+                !username ||
+                !password
+              }
+            >
+              Create Account
+            </button>
+          </form>
         </div>
-        <div className="form-group">
-          <input
-            value={phone}
-            className="form-control"
-            type="text"
-            placeholder="phone number"
-            onChange={(ev) => setPhone(ev.target.value)}
-          />
+      </header>
+      <section className="section section-a">
+        <div className="container">
+          <h2>WannaHang</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde,
+            impedit amet minima iste autem cumque et maiores blanditiis
+            doloribus aut dolorum quaerat non est voluptatum, tempore ut dolorem
+            voluptas quod quae accusantium, ex inventore ducimus. Beatae
+            mollitia exercitationem, quam similique, consectetur ratione
+            reprehenderit delectus neque eligendi facere soluta dolor ducimus!
+          </p>
+          <p>©2020 The TEC Group</p>
         </div>
-        <div className="form-group">
-          <input
-            value={email}
-            type="email"
-            className="form-control"
-            id="inputEmail1"
-            aria-describedby="emailHelp"
-            placeholder="email address"
-            onChange={(ev) => setEmail(ev.target.value)}
-          />
-          <small id="emailHelp" className="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
-        </div>
-        <div className="form-group">
-          <input
-            value={password}
-            type="password"
-            className="form-control"
-            id="inputPassword1"
-            placeholder="password"
-            onChange={(ev) => setPassword(ev.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            value={confirmPassword}
-            type="password"
-            className="form-control"
-            id="inputConfirmPassword1"
-            placeholder="confirm password"
-            onChange={(ev) => setConfirmPassword(ev.target.value)}
-          />
-          <div>{passwordError}</div>
-        </div>
-        <button
-          className="btn btn-primary"
-          disabled={
-            !firstname ||
-            !lastname ||
-            !email ||
-            !phone ||
-            !username ||
-            !password
-          }
-        >
-          Create Account
-        </button>
-      </form>
+      </section>
     </div>
   );
 };
