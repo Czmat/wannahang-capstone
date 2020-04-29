b00b3bff057a492c37d07eff5d01a0774d733f1d
Chaise Matev <aj_fl81@msn.com>
2020-04-28 20:28:55 -0400

    commit after merge

--- db/models/favorites.js
+++ db/models/favorites.js
@@ -15,22 +15,22 @@ const favorites = {
   },
   update: async ({ userId }, id) => {
     const SQL = `UPDATE `;
-    // const findFavorites = async (userid) => {
-    //   const SQL = `SELECT * from user_favorites WHERE userid = $1`;
-    //   return (await client.query(SQL, [userid])).rows;
-    // };
-
-    // const createFavorite = async (userfavorite) => {
-    //   const SQL = `INSERT INTO user_favorites("userId", "favoriteId")
-    //         values($1, $2) returning *`;
-    //   return (
-    //     await client.query(SQL, [userfavorite.userId, userfavorite.favoriteId])
-    //   ).rows[0];
-    // };
-
     const updatedEvent = (await client.query(SQL, [userId, id])).rows[0];
     return updatedEvent;
   },
+  // const findFavorites = async (userid) => {
+  //   const SQL = `SELECT * from user_favorites WHERE userid = $1`;
+  //   return (await client.query(SQL, [userid])).rows;
+  // };
+
+  // const createFavorite = async (userfavorite) => {
+  //   const SQL = `INSERT INTO user_favorites("userId", "favoriteId")
+  //         values($1, $2) returning *`;
+  //   return (
+  //     await client.query(SQL, [userfavorite.userId, userfavorite.favoriteId])
+  //   ).rows[0];
+  // };
+
   delete: async (id) => {
     return (
       await client.query(
